49060d94c04fae65884b2191ce6172a54c46d8e3
sohyerim-dev <fnmnn1@gmail.com>
2025-11-10 15:11:51 +0900

    prettier, eslint setting

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@ import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import { defineConfig } from "eslint/config";
+import prettierConfig from "eslint-config-prettier";
 
 export default defineConfig([
   {
@@ -16,10 +17,12 @@ export default defineConfig([
       "no-var": "off", // var 키워드 사용 가능
       "prefer-const": "warn", // 변수가 재할당 되지 않는다면 let 대신 const 사용하도록 경고
       "no-redeclare": 1, // 변수 중복 선언시 경고
+      quotes: ["error", "double"], // double quote 사용
       // 기타 룰 추가
     },
   },
   {
     ignores: [".history", "dist"], // ignores 속성만 제공하면 전체 설정에 적용
   },
+  prettierConfig.recommended,
 ]);
