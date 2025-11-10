65172ab13a91563937ccacc3aa68cf9eddbc3a7e
sohyerim-dev <fnmnn1@gmail.com>
2025-11-10 16:29:18 +0900

    eslint setting update

--- eslint.config.js
+++ eslint.config.js
@@ -14,11 +14,15 @@ export default defineConfig([
   tseslint.configs.recommended,
   {
     rules: {
-      "no-var": "off", // var 키워드 사용 가능
+      "no-var": "on", // var 키워드 사용 불가능
       "prefer-const": "warn", // 변수가 재할당 되지 않는다면 let 대신 const 사용하도록 경고
       "no-redeclare": 1, // 변수 중복 선언시 경고
       quotes: ["error", "double"], // double quote 사용
+      "no-unused-vars": "warn", // 사용 안 하는 변수 경고
+      semi: ["error", "always"], // 세미콜론 강제
+
       // 기타 룰 추가
+      // "no-console": "warn", // 프로젝트 배포 시 console 남겨두지 않기
     },
   },
   {
