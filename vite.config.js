0773488582df843d77e696ab2f80fefa8c18a5a1
sohyerim-dev <fnmnn1@gmail.com>
2025-11-13 07:46:10 +0900

    feat: 글쓰기 페이지 스타일 완성

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,19 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  server: {
+    host: true,
+    port: 5173,
+  },
   build: {
     rollupOptions: {
       input: {
         // 메인 페이지
         index: 'index.html',
+        // 글쓰기 페이지
+        'pages/write': 'src/pages/write.html',
+        // 상세 페이지
+        'pages/detail': 'src/pages/detail.html',
       },
     },
   },
