bc53853a873373a9af9267af00d1006350858758
park-yeseul <banasseul@gmail.com>
2025-11-18 16:57:06 +0900

    feat: 경로 수정

--- vite.config.js
+++ vite.config.js
@@ -12,19 +12,15 @@ export default defineConfig({
         index: 'index.html',
         // 로그인 페이지
         'pages/login-page/login': 'src/pages/login-page/login.html',
-        // 카카오 로그인 페이지
-        'pages/login-page/login-kakao': 'src/pages/login-page/login-kakao.html',
-        // 카카오 간편로그인 페이지
-        'pages/login-page/login-kakao-easy':
-          'src/pages/login-page/login-kakao-easy.html',
         // 회원가입 페이지
         'pages/login-page/login-join': 'src/pages/login-page/login-join.html',
         // 내 서랍 페이지
         'pages/myinfo/my-info': 'src/pages/myinfo/my-info.html',
         // 작가 홈 페이지
-        'pages/writer-home': 'src/pages/writer-home.html',
+        'pages/writer-home-page/writer-home':
+          'src/pages/writer-home-page/writer-home.html',
         // 발견 페이지
-        'pages/search': 'src/pages/search.html',
+        'pages/search-page/search': 'src/pages/search-page/search.html',
         // 글쓰기 페이지
         'pages/write-page/write': 'src/pages/write-page/write.html',
         // 상세 페이지
